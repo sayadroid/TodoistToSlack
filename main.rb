d0517c576e931fa53f3d3f7306739e40343e1849
SayaTsutsumi <saya.tsutsumi@gmail.com>
2015-01-21 21:00:13 +0900

    ガイドコメント追加

--- main.rb
+++ main.rb
@@ -4,5 +4,7 @@ require './lib/slack.rb'
 message = Message.new
 
 slack = Slack.new
+# 本日のタスク通知
 slack.post_message(message.getTodayTasks());
+# 本日完了したタスクと明日のタスクセット通知（使うときは下記１行のコメントアウト外してください）
 # slack.post_message(message.getTomorrowTasksAndCompleyedTasks());
